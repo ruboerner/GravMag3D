fb4e66e7964fd683fc13ad5bd95147fe42d11d2b
Ralph-Uwe Börner <rub@geophysik.tu-freiberg.de>
2022-07-19 14:33:50 +0200

    - Add function argument validation - Add link to original method

--- TriAngle.m
+++ TriAngle.m
@@ -1,4 +1,24 @@
 function ang = TriAngle(A,B,C)
+% Triangle computes solid angle spanned by a triangle in R3 as seen from from origin
+%
+% ang = TriAngle(A,B,C)
+% 
+% Input:
+% =====
+% A, B, C: (3 x 1) arrays of triangle vertices ordered counterclockwise
+% 
+% Output:
+% ======
+%
+% ang: Solid angle in rad
+%
+% Implemented after Osteroom and Stracke (1983) (https://doi.org/10.1109%2FTBME.1983.325207)
+%
+arguments
+    A (3, 1)
+    B (3, 1)
+    C (3, 1)
+end
 An = norm(A);
 Bn = norm(B);
 Cn = norm(C);
